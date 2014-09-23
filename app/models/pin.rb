3b6f88f59b4513c686b35cf6b7794c8dded8cb34
Gustavo Varallo <gustavo.varallo@live.com>
2014-09-23 11:51:31 -0300

    Add title to pins and translations update

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,8 +1,11 @@
 class Pin < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+  has_attached_file :image, :styles => { :medium => "600x600>", :thumb => "100x100>" }
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"], 
+                                    :size => { :in => 0..10.kilobytes }
   
   validates :image, presence: true
+  validates :title, length: { minimum: 3, maximum: 50, too_short: "não pode ter menos que %{count} caracteres", 
+                                          too_long: "não pode ter mais que %{count} caracteres" }
   validates :description, presence: true
 end
